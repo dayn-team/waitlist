9444436b291b8e7fb58e0bd3b2abb043ff2913ed
eliazino <Elias.akin@hotmail.com>
2024-11-11 09:32:04 +0100

    Project Files

---
+++ WebAPI/ExceptionHelper/ExceptionMessage.cs
@@ -0,0 +1,64 @@
+﻿using Core.Application.Errors;
+using Core.Application.Interfaces.Infrastructure.Identity;
+using Core.Domain.DTOs.Configurations;
+using Core.Domain.DTOs.Others;
+using Core.Domain.DTOs.Response;
+
+namespace WebAPI.ExceptionHelper {
+    public class ExceptionMessage {
+        private readonly SystemVariables _var;
+        private readonly ILogger _logger;
+        private readonly IIdentityManager _identity;
+        public int statusCode { get; private set; } = 200;
+        WebResponse<object> response = new WebResponse<object>();
+        public ExceptionMessage(SystemVariables _var, ILogger? logger, IIdentityManager id) {
+            this._var = _var;
+            _logger = logger;
+            _identity = id;
+        }
+        public WebResponse<object> getMessage(Exception _exc) {
+            if (_exc is AuthenticationError) {
+                statusCode = 401;
+                return response.fail(ResponseCodes.ACCESS_DENIED_ERROR, _exc.Message);
+            }
+            if (_exc is InputError) {
+                var respObj = response.fail(ResponseCodes.INPUT_ERROR, _exc.Message);
+                statusCode = respObj.statusCode;
+                return respObj;
+            }
+            if (_exc is LogicError) {
+                var respObj = response.fail(ResponseCodes.LOGIC_ERROR, _exc.Message);
+                statusCode = respObj.statusCode;
+                return respObj;
+            }
+            if (_exc is ServiceError) {
+                var respObj = response.fail(ResponseCodes.SERVICE_ERROR, _exc.Message);
+                statusCode = respObj.statusCode;
+                return respObj;
+            }
+            if (_exc is NotFoundError) {
+                var respObj = response.fail(ResponseCodes.RESOURCE_NOT_FOUND, _exc.Message);
+                statusCode = respObj.statusCode;
+                return respObj;
+            }
+
+            statusCode = 500;
+
+            string deb = _identity.getHeaderValue("debug");
+            if (_var.debug || !string.IsNullOrEmpty(deb)) {
+                if (deb == "one") {
+                    return response.fail(ResponseCodes.SYSTEM_ERROR, _exc.ToString());
+                } else {
+                    return response.fail(ResponseCodes.SYSTEM_ERROR, _exc.ToString());
+                }
+            }
+            if (!(_logger is null)) {
+                string route = _identity.endPointAddress;
+                string request = _identity.IPAddress;
+                var logData = new ErrLogDTO { details = _exc.ToString(), request = request, route = route };
+                _logger.LogError("Request {route} Throws {@Position}", route, logData);
+            }
+            return response.fail(ResponseCodes.SYSTEM_ERROR, "Service is unavailable. Try again after some time or contact admin");
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,32 +1,56 @@
-var builder = WebApplication.CreateBuilder(args);
+using AutoMapper;
+using Core.Domain.DTOs.Configurations;
+using Microsoft.AspNetCore.ResponseCompression;
+using Newtonsoft.Json.Linq;
+using StackExchange.Redis.Extensions.Core.Configuration;
 
+var builder = WebApplication.CreateBuilder(args);
+var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+string fileName = string.Concat("appsettings.", builder.Environment.EnvironmentName, ".json");
+configBuilder.AddJsonFile(fileName, optional: true);
+var Configuration = configBuilder.Build();
+var appSettingsSection = Configuration.GetSection("SystemVariables");
+builder.Services.Configure<SystemVariables>(appSettingsSection);
+var appSettings = appSettingsSection.Get<SystemVariables>();
 // Add services to the container.
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+builder.Services.AddCors(options => {
+    options.AddPolicy("CorsPolicy",
+    builder => builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 });
 
+builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddResponseCompression(Options => {
+    Options.EnableForHttps = true;
+    Options.Providers.Add<GzipCompressionProvider>();
+});
+var t = appSettingsSection.GetSection("Redis").Get<RedisConfiguration>();
+builder.Services.AddStackExchangeRedisCache(options => {
+    options.Configuration = $"{t.Hosts[0].Host}:{t.Hosts[0].Port},password={t.Password},ssl={t.Ssl},abortConnect={t.AbortOnConnectFail}";
+});
+var cnfg = new MapperConfiguration(cfg => {
+    cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+    cfg.CreateMap<JValue, object>().ConvertUsing(source => source.Value);
+});
+var mapper = cnfg.CreateMapper();
+builder.Services.AddSingleton(mapper);
+Infrastructure.DI.Resolve.resolve(builder.Services);
+Core.Application.DI.Resolve.resolve(builder.Services);
+builder.Services.AddControllers();
+var app = builder.Build();
+app.UseWebSockets();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.UseCors(builder =>
+                builder.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              );
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
